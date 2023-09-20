c7845657ba597a001545d7d8010973d547d3ddaa
kschang15 <kschang15@naver.com>
2023-09-20 18:02:40 +0900

    [Bronze 2] 음계 - 2920

---
+++ solved.ac/Bronze/2920. 음계/2920.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int asc, des, mix, scale[100];
+    for (int i=1; i<=8; i++)
+    {
+        cin >> scale[i];
+    }
+
+}
