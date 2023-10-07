16aed703d94d6c76061a681bc25d1bf6285e4019
kschang15 <kschang15@naver.com>
2023-10-07 19:38:36 +0900

    [Bronze 2] Hashing - 15829

--- solved.ac/Bronze/15829. Hashing/15829.cc
+++ solved.ac/Bronze/15829. Hashing/15829.cc
@@ -1,8 +1,21 @@
 #include <iostream>
+#define mod 1234567891
 using namespace std;
 
-int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    long long L, sum = 0, hash = 1;
+    string input;
+    cin >> L >> input;
+    for (int i = 0; i < L; i++)
+    {
+        sum += (input[i] - 'a' + 1) * hash;
+        hash = (hash * 31) % mod;
+        sum %= mod;
+    }
+    cout << sum;
 }
