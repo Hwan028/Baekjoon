7d3018045090f5fd437fc829cff5f5eb97bdd6d0
kschang15 <kschang15@naver.com>
2023-11-01 18:02:15 +0900

    [Silver 5] 좌표 정렬하기 - 11650

---
+++ solved.ac/Silver/11650. 좌표 정렬하기/11650.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b) {
+  if (a.first == b.first)
+    return a.second < b.second;
+  return a.first < b.first;
+}
+
+int main() {
+  int N;
+  cin >> N;
+  vector<pair<int, int>> ioput(N);
+
+  for (int i = 0; i < N; i++)
+    cin >> ioput[i].first >> ioput[i].second;
+
+  sort(ioput.begin(), ioput.end(), compare);
+  
+  for (int i = 0; i < N; i++)
+    cout << ioput[i].first << " " << ioput[i].second << "\n";
+}
